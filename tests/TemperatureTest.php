1f40dfe5d81663e50243e4d7199bb155d71924f2
rafa1944 <rafa1944@users.noreply.github.com>
2020-12-05 16:45:23 +0000

    Fix styling

--- tests/TemperatureTest.php
+++ tests/TemperatureTest.php
@@ -10,7 +10,6 @@ class TemperatureTest extends TestCase
     /** @test */
     public function it_can_convert_celsius_to_fahrenheit()
     {
-
         $fahrenheit = Temperature::fromCelsius(100)->toFahrenheit();
 
         $this->assertEquals(212, $fahrenheit);
